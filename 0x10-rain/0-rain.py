e5956c308b39a90496f9c2b65dd63d0873c7821a
inoublii <inoublii@gmail.com>
2021-01-11 19:47:46 +0000

    rain

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def rain(walls):
+    """calculate how much water will be
+    retained after rain
+    """
+    if len(walls) == 0:
+        return 0
+
+    water = 0
+    l = len(walls)
+    for n in range(l):
+        left = walls[n]
+        for i in range(n):
+            left = max(left, walls[i])
+        right = walls[n]
+        for i in range(n + 1, l):
+            right = max(right, walls[i])
+        water +=  min(left, right) - walls[n]
+    return water
