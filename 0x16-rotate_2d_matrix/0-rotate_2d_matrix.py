a24216c3587ca06a64f7b972ec9bed61aad86851
inoublii <inoublii@gmail.com>
2021-01-26 20:43:26 +0000

    matrix rotation

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Given an n x n 2D matrix, rotate it 90 degrees clockwise """
+
+
+def rotate_2d_matrix(matrix):
+    """ Given an n x n 2D matrix, rotate it 90 degrees clockwise """
+    x = len(matrix)
+    lists = []
+    for i in range(x):
+        sub = []
+        for q in range(x):
+            sub.append(matrix[i][x])
+        lists.append(sub)
+    for i in range(x):
+        for q in range(x):
+            matrix[i][q] = sub[x - i - 1][q]
