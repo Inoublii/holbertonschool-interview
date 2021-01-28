ec6a5fa45a81e9e9f39332c40e0d36d2174d59d8
inoublii <inoublii@gmail.com>
2021-01-28 22:09:56 +0000

    infinite multiplication

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,80 +1,107 @@
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c);
 /**
- * main -  multiplies two positive numbers
+ * _strlen - strlen custom function
+ * @str: string
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+i++;
+return (i++);
+}
+
+/**
+ * err - display error
+ * Return: just print
+ */
+void err(void)
+{
+char *error = "Error";
+int i;
+
+for (i = 0; error[i] != '\0'; i++)
+_putchar(error[i]);
+_putchar('\n');
+}
+/**
+ * main - Store the arguments
+ *
  * @argc: number of arguments
- * @argv: arguments list
- * Return: 98 if error, 0 if success
+ * @argv: value of arguments
+ *
+ * Return: Display number of arguments
  */
 int main(int argc, char *argv[])
 {
-char *i, *j;
-char *a, *z;
+int a[10000], b[10000];
+int ans[10000] = {0};
+int i, j, tmp;
+char *s1, *s2;
+int l1;
+int l2;
 
-long int l1, l2;
-
-if (argc != 3)
+if (argc == 3)
 {
-write(1, "Error\n", 6);
-return (98);
+s1 = argv[1];
+s2 = argv[2];
+int x, z;
+for (x = 0; s1[x] != '\0'; x++)
+{
+if (s1[x] < '0' || s1[x] > '9')
+z = 98;
 }
-l1 = 0;
-for (i = argv[1]; *i != 0; i++, l1++)
-if (*i < '0' || *i > '9')
+z = 0;
+if (z == 98)
 {
-write(1, "Error\n", 6);
-return (98);
+err();
+exit(98);
 }
-l2 = 0;
-for (j = argv[2]; *j != 0; j++, l2++)
-if (*j < '0' || *j > '9')
+for (x = 0; s2[x] != '\0'; x++)
 {
-write(1, "Error\n", 6);
-return (98);
+if (s2[x] < '0' || s2[x] > '9')
+z = 98;
 }
-if (l1 == 0 || l2 == 0)
+z = 0;
+if (z == 98)
 {
-write(1, "Error\n", 6);
-return (98);
+err();
+exit(98);
 }
-for (a = argv[1]; *a == '0' && a[1] != 0; l1--)
-a++;
-for (z = argv[2]; *z == '0' && z[1] != 0; l2--)
-z++;
+l1 = _strlen(s1);
+l2 = _strlen(s2);
+for (i = l1 - 1, j = 0; i >= 0; i--, j++)
+a[j] = s1[i] - '0';
+for (i = l2 - 1, j = 0; i >= 0; i--, j++)
+b[j] = s2[i] - '0';
+for (i = 0; i < l2; i++)
 {
-long int i, j, r;
-int carry;
-char *result, *beg;
-
-result = malloc(l1 + l2 + 1);
-if (result == NULL)
-{
-write(1, "Error\n", 6);
-return (98);
+for (j = 0; j < l1; j++)
+ans[i + j] += b[i] * a[j];
 }
-for (i = 0; i < l1 + l2 + 3; i++)
-result[i] = 0;
-for (i = l1 - 1; i >= 0; i--)
-for (j = l2 - 1; j >= 0; j--)
+for (i = 0; i < l1 + l2; i++)
 {
-result[i + j + 1] += ((a[i] - '0') *
-    (z[j] - '0'));
-for (r = i + j + 1; result[r] > 9;
-r--)
+tmp = ans[i] / 10;
+ans[i] = ans[i] % 10;
+ans[i + 1] = ans[i + 1] + tmp;
+}
+for (i = l1 + l2; i >= 0; i--)
 {
-carry = result[r] / 10;
-result[r - 1] += carry;
-result[r] %= 10;
+if (ans[i] > 0)
+break;
 }
-}
-for (i = l1 + l2 - 1; i >= 0; i--)
-result[i] += '0';
-for (beg = result; *beg == '0' && beg[1] != 0; beg++)
-l1--;
-l1 += l2;
-write(1, beg, l1);
-write(1, "\n", 1);
-free(result);
+if (i == -1)
+_putchar(ans[0] + 48);
+for (; i >= 0; i--)
+_putchar(ans[i] + 48);
+_putchar('\n');
 return (0);
 }
+err();
+exit(98);
 }
