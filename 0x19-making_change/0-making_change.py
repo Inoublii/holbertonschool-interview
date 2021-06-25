51cfb457b37f2b5c6a3e6857d940bad8cf6a5415
inoublii <inoublii@gmail.com>
2021-06-25 23:30:03 +0100

    Change comes from within

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""makeChange function."""
+
+def makeChange(coins, total):
+    """Returns the fewest number of coins needed to meet a given total"""
+    if total <= 0:
+        return 0
+    res = [total + 1] * (total + 1)
+    res[0] = 0
+    for a in range(1, total + 1):
+        for b in range(0, len(coins)):
+            if coins[b] <= a:
+                res[a] = min(res[a], res[a - coins[b]] + 1)
+    return -1 if res[total] > total else res[total]
+
